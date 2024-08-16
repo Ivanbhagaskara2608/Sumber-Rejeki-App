69918f63fabf4c87bf2c91cf75ee07e7b34225db
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2024-08-16 13:34:51 +0700

    commit

--- lib/pages/add_transaction.dart
+++ lib/pages/add_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AddTransactionPage extends StatefulWidget {
   const AddTransactionPage({super.key});
@@ -11,22 +12,36 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-        backgroundColor: Colors.blueAccent,
-        centerTitle: true,
-        title: const Text(
-          'Tambah Transaksi',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+          title: const Text(
+            'Tambah Transaksi',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      body: const Center(
-        child: Text('Add Transaction Page'),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.grey[200]),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        hintText: 'Nama Barang'),
+                  ))
+            ],
+          ),
+        ));
   }
 }
